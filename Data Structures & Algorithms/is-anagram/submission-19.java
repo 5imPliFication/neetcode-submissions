f6621976db9de1c98d2876f494a1e21b75544217
Nguyen Bao Nam <130468760+5imPliFication@users.noreply.github.com>
2026-09-08 13:25:20 +0700

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> h1 = new HashMap<>();
+        HashMap<Character, Integer> h2 = new HashMap<>();
+        char[] a1 = s.toCharArray();
+        char[] a2 = t.toCharArray();
+        for (int i = 0; i < a1.length; i++) {
+            if (!h1.containsKey(a1[i])) {
+                h1.put(a1[i], 1);
+            }
+            if (h1.containsKey(a1[i])) {
+                h1.put(a1[i], h1.getOrDefault(a1[i], 1) + 1);
+            }
+        }
+        for (int j = 0; j < a2.length; j++) {
+            if (!h2.containsKey(a2[j])) {
+                h2.put(a2[j], 1);
+            }
+
+            if (h2.containsKey(a2[j])) {
+                h2.put(a2[j], h2.getOrDefault(a2[j], 1) + 1);
+            }
+        }
+
+        return h1.equals(h2);
+    }
+}
