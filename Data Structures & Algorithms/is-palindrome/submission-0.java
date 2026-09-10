d76ca9f058c7131efca86848791f04d18cecf47a
Nguyen Bao Nam <130468760+5imPliFication@users.noreply.github.com>
2026-09-10 13:23:27 +0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        char[] str = s.toCharArray();
+        int n = str.length;
+        int l = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (str[i] == str[n - 1 - i])
+                count++;
+        }
+        return count == n;
+    }
+}
